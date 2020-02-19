2b6447de1d957bc8d3724a8b714426cd75f5f54c
Park Miso <alth7512@gmail.com>
2020-02-20 03:53:00 +0900

    Custom User Model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,41 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    """ Custom User Model """
+
+    GENDER_MALE = 'male'
+    GENDER_FEMALE = 'female'
+    GENDER_OTHER = 'other'
+    GENDER_CHOICES = (
+        (GENDER_MALE, 'Male'),
+        (GENDER_FEMALE, 'Female'),
+        (GENDER_OTHER, 'Other'),
+    )
+
+    LAN_ENG = 'en'
+    LAN_KOR = 'kr'
+    LAN_CHOICES = (
+        (LAN_ENG, 'English'),
+        (LAN_KOR, 'Korean')
+    )
+
+    CURRENCY_USD = 'usd'
+    CURRENCY_KRW = 'krw'
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, 'USD'),
+        (CURRENCY_KRW, 'KRW')
+    )
+
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES,
+                              max_length=10, null=True, blank=True)
+    bio = models.TextField(default="")
+    birthdate = models.DateField(null=True)
+    langauge = models.CharField(
+        choices=LAN_CHOICES, max_length=2, null=True, blank=True)
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=3, null=True, blank=True)
+    superhost = models.BooleanField(default=False)
