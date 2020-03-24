6510cd2f5d24b281261359ebe698869e2f25aa6c
Park Miso <alth7512@gmail.com>
2020-03-24 19:17:59 +0900

    github login template

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,9 @@ app_name = "login"
 
 urlpatterns = [
     path("login", views.LoginView.as_view(), name="login"),
+    path("login/github", views.github_login, name="github-login"),
+    path("login/github/callback", views.github_callback,
+         name="github-login-callback"),
     path("logout", views.log_out, name="logout"),
     path("signup", views.SignUpView.as_view(), name="signup"),
     path("verify/<str:key>", views.complete_verification,
